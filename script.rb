1e152b7ec0a1eb1c9a63ac4d393b4cf47fc5859f
Eduardo Bonilla <eduardobonillac@gmail.com>
2019-11-28 16:59:29 -0600

    rubocopcomments fixed

--- script.rb
+++ script.rb
@@ -6,60 +6,67 @@ module Enumerable
       i += 1
     end
   end
+
   def my_each_with_index
     i = 0
-    self.length.times do
+    length.times do
       yield(self[i], i)
       i += 1
     end
   end
+
   def my_select
     i = 0
     res_arr=[]
-    self.length.times do
+    length.times do
       res_arr << self[i] if yield(self[i]) == true
       i += 1
     end
-    return res_arr
+    res_arr
   end
+
   def my_all?
     i = 0
-    self.length.times do
+    length.times do
       return false if yield(self[i]) == false
       i += 1
     end
-    return true
+    true
   end
+
   def my_none?
     i = 0
-    self.length.times do
+    length.times do
       return false if yield(self[i]) == true
       i += 1
     end
-    return true
+    true
   end
+
   def my_count
     i = 0
     c=0
-    self.length.times do
+    length.times do
       c+=1 if yield(self[i]) == true
       i += 1
     end
-    return c
+    c
   end
+
   def my_map
     i = 0
     res_Array=[]
-    self.length.times do
+    length.times do
       res_Array << yield(self[i]) if block_given?
       res_Array <<  proc.call(self[i])
       i += 1
     end
-    return res_Array
+    res_Array
   end
+
   def my_inject start_value
-    self.my_each{|element| start_value = yield(start_value,element)}
-    return start_value
+    my_each{|element| start_value = yield(start_value,element)}
+    start_value
   end
 end
 def multiply_els(arr)
@@ -72,3 +79,4 @@ testd=[2,4,5]
 my_proc = Proc.new {|i| i*2}
 print testa.my_map(&my_proc)
 print testa.my_map{|i| i*2}
+print testa.my_each{|i| i*2}
